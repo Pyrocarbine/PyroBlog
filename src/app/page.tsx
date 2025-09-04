24a33ccc3e54e0dab0c260edfb732c4d85d3f264
Pyrocarbine <aaronhan2006@gmail.com>
2025-09-03 22:24:49 -0400

    added apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const router = useRouter();
   return (
     <main>
-      <div className="text-center text-3xl">Aaron's Blogger</div>
+      <div className="text-center text-3xl">Aaron&apos;s Blogger</div>
       <div className="text-center text-base pt-3 pb-3"> Welcome to my blogs</div>
       <button onClick={() => router.push("/new-post")} className="w-lg block text-base rounded-sm border border-blue-400  bg-blue-400 text-white text-center pt-3 pb-3 m-auto"> Create New Blog</button>
     </main>
