8eadcf3057017689091d8db1e7fe0ccdae01b2b7
Pyrocarbine <aaronhan2006@gmail.com>
2025-09-08 00:21:30 -0400

    implemented google authentication

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
+import SignIn from "./sign-in";
 
 export default function NavBar() {
-    return (
-        <div className="w-full h-16 border-b bg-white text-gray-800 flex items-center justify-center mb-5">
-            <h1 className="text-2xl text-gray-800 font-bold"><Link href="/">PyroBlog</Link></h1>
-        </div>
-    );
+  return (
+    <div className="w-full h-16 border-b bg-white text-gray-800 flex items-center px-4 mb-5">
+      {/* Left: site title */}
+      <h1 className="text-2xl font-bold translate-x-10">
+        <Link href="/">PyroBlog</Link>
+      </h1>
+
+      {/* Spacer to push SignIn to the right */}
+      <div className="flex-1" />
+
+      {/* Right: SignIn button */}
+      <SignIn />
+    </div>
+  );
 }
