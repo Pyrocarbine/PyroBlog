a22b2492cdfc80da4637473c4034160794b39a30
Pyrocarbine <aaronhan2006@gmail.com>
2025-09-13 17:17:01 -0400

    changed session import

--- src/app/api/make-post/route.ts
+++ src/app/api/make-post/route.ts
@@ -3,14 +3,14 @@ import { neon } from '@neondatabase/serverless';
 import { auth } from "../../auth"
 
 const sql = neon(process.env.DATABASE_URL!);
-type Session = import("/Users/aaronhan/Projects/React/PyroBlog/blog/node_modules/@auth/core/types").Session;
+type Session = import("@auth/core/types").Session;
 
 export async function POST(req: Request) {
   const session : Session | null = await auth();
   if (!session?.user) return NextResponse.json({ success: false, error: "User is not signed in. Please sign in to make new posts"}, { status: 500 });
   const displayName : string = session.user.name || "Anonymous";
   try {
-    const { title, content } = await req.json();
+    const { title, content } = await req.json() as { title: string; content: string };
     const [postID] = await sql`INSERT INTO posts (title, content, user_id, email, display_name) VALUES (${title}, ${content}, ${session.user.id}, ${session.user.email}, ${displayName}) RETURNING id`;
     return NextResponse.json({ success: true, postId: postID.id});
   } catch (error) {
