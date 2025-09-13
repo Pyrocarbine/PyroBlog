ea20b4a21ce8edc333298de3ed985e9e1bb2d2b0
Pyrocarbine <aaronhan2006@gmail.com>
2025-09-13 17:13:47 -0400

    fixed deletepost

--- src/app/api/delete-post/[id]/route.ts
+++ src/app/api/delete-post/[id]/route.ts
@@ -1,15 +1,10 @@
 import { NextResponse } from 'next/server';
 import { neon } from '@neondatabase/serverless';
-import { QueryResult } from 'pg';
-import { auth } from "../../../auth"
-import { Post } from '../../../types/post';
-
 const sql = neon(process.env.DATABASE_URL!);
 
-export async function DELETE(req: Request, {params}: {params: {id: string}}) {
-    const p = await params;
-    const postId = p.id;
-    const request = await sql`DELETE FROM posts WHERE id = ${postId}`;
+export async function DELETE(req: Request, {params}: {params: Promise<{id: string}>}) {
+    const { id }  = await params;
+    const request = await sql`DELETE FROM posts WHERE id = ${id}`;
     return NextResponse.json({ success: true });
 }
 
